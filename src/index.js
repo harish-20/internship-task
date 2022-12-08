b53be60236d3091f57b5a51e3f2f0a5afbd7e5e9
harish kumar <harishkumarharishkumar108@gmail.com>
2022-12-08 23:36:53 +0530

    persisted store

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import App from './App'
-import store from './store/store'
+import store, { persistedStore } from './store/store'
 
 import './index.css'
 
@@ -12,10 +13,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistedStore}>
+      <BrowserRouter>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
 )
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { cartReducer } from './Slices/cartSlice'
+import storage from 'redux-persist/lib/storage'
+import persistReducer from 'redux-persist/es/persistReducer'
+import persistStore from 'redux-persist/es/persistStore'
+import thunk from 'redux-thunk'
 
-const store = configureStore({ reducer: cartReducer })
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, cartReducer)
+
+const store = configureStore({ reducer: persistedReducer, middleware: [thunk] })
+
+export const persistedStore = persistStore(store)
 
 export default store
